0bd6d1d245eb88270cabb53e4332a51930b71f36
Jiwon-Park <wldnjs1323@naver.com>
2018-10-05 12:28:04 +0900

    rev 181005:12:26

--- app/src/main/java/edu/skku/sparkdec/sparkdec/MainActivity.java
+++ app/src/main/java/edu/skku/sparkdec/sparkdec/MainActivity.java
@@ -1,13 +1,149 @@
 package edu.skku.sparkdec.sparkdec;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_map) {
+            // Handle the camera action
+        } else if (id == R.id.nav_statistics) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void test(View v) throws UnsupportedEncodingException {
+        if (android.os.Debug.isDebuggerConnected())
+            android.os.Debug.waitForDebugger();
+        ArrayList<Double[]> result = pedestrianPath("1", "1", "1", "1", "출발지", "도착지");
+        String a = Double.toString(result.get(0)[0]) + " " + Double.toString(result.get(0)[1]) + "\n";
+        for (int i = 1; i < result.size(); i++) {
+            a += Double.toString(result.get(i)[0]) + " " + Double.toString(result.get(i)[1]) + "\n";
+        }
+        Toast.makeText(this, a, Toast.LENGTH_LONG);
+        System.out.println(a);
+    }
+
+    public ArrayList<Double[]> pedestrianPath(String sx, String sy, String ex, String ey, String startName, String endName) throws UnsupportedEncodingException {
+        final String coordinate = "WGS84GEO";
+        final String option = "0";
+        Long epochTime = System.currentTimeMillis() / 1000;
+        epochTime -= 31556926 * 33;
+        final String gpsTime = epochTime.toString();
+        final String requestValue = "startX=" + "127.132707" + "&startY=" + "37.611132" + "&endX=" + "126.977525" + "&endY=" + "37.291691" + "&reqCoordType=" + coordinate +
+                "&startName=" + URLEncoder.encode(startName, "UTF-8") + "&endName=" + URLEncoder.encode(endName, "UTF-8") + "&searchOption=" + option + "&resCoordType=" + coordinate;
+
+        System.out.println(requestValue);
+        try {
+            String returnString = new TmapPedestrian().execute(requestValue).get();
+            ArrayList<Double[]> returnValue = new ArrayList<>();
+            JSONObject jParser = new JSONObject(returnString);
+            JSONArray jArray = jParser.getJSONArray("features");
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject obj = jArray.getJSONObject(i);
+
+                if (obj.getJSONObject("geometry").getString("type").equals("Point")) {
+                    JSONArray innerArray = obj.getJSONObject("geometry").getJSONArray("coordinates");
+                    Double[] doubleArray = {innerArray.getDouble(0), innerArray.getDouble(1)};
+                    returnValue.add(doubleArray);
+                }
+            }
+            return returnValue;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
+
